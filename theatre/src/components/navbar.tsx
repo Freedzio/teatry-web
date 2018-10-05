d669a7031cf1960d1d4a31896aa77a69f5cfc7db
Tomasz Łuniewski <luniewski.tomasz@gmail.com>
2018-10-05 14:31:26 +0200

    added bootstrap files

---
+++ theatre/src/components/navbar.tsx
@@ -0,0 +1,10 @@
+//pseudo <code>
+//1. po lewej stronie paska nazwa systemu ktory jest linkiem do indexu 
+//2. drugi element to odnosnik doi listy teatrow
+//3. trzeci element to link do listy spektatkli
+//4. prawa strona - zaloguj i zarejestruj
+
+import * as React from 'react';
+
+
+
